040edd2119a52a65ab095e17c9d18b0e8c4c1b05
YCheng <briansam195@gmail.com>
2025-03-07 15:50:25 +0800

    新增 Pinia 模組支援，更新配置與相依性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/index.css"],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  modules: ["@pinia/nuxt"],
 });
