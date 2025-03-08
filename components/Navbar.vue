a9a1d8c2fbae674db443adef13f7c8122b84b225
YCheng <briansam195@gmail.com>
2025-03-08 21:34:43 +0800

    新增導航欄元件，更新首頁與動態路由頁面結構

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="text-3xl font-bold text-red-500">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/news/business">Business</NuxtLink>
+    <NuxtLink to="/news/entertainment">Entertainment</NuxtLink>
+    <NuxtLink to="/news/general">General</NuxtLink>
+    <NuxtLink to="/news/health">Health</NuxtLink>
+    <NuxtLink to="/news/science">Science</NuxtLink>
+    <NuxtLink to="/news/sports">Sports</NuxtLink>
+    <NuxtLink to="/news/technology">Technology</NuxtLink>
+    <NuxtLink to="/view/123">View 123</NuxtLink>
+  </nav>
+</template>
+
+<style scoped></style>
