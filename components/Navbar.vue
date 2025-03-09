c28a1eabed75634f458e3a4715fa667d85932117
YCheng <briansam195@gmail.com>
2025-03-09 22:23:59 +0800

    更新導航欄元件，使用動態路由生成連結並調整樣式

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,17 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = [
+  { name: "首頁", path: "/" },
+  { name: "商業", path: "/news/business" },
+  { name: "娛樂", path: "/news/entertainment" },
+  { name: "綜合", path: "/news/general" },
+  { name: "健康", path: "/news/health" },
+  { name: "科學", path: "/news/science" },
+  { name: "體育", path: "/news/sports" },
+  { name: "科技", path: "/news/technology" },
+  { name: "測試", path: "/news/test" },
+];
+</script>
 
 <template>
-  <nav class="text-3xl font-bold text-red-500">
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/news/business">Business</NuxtLink>
-    <NuxtLink to="/news/entertainment">Entertainment</NuxtLink>
-    <NuxtLink to="/news/general">General</NuxtLink>
-    <NuxtLink to="/news/health">Health</NuxtLink>
-    <NuxtLink to="/news/science">Science</NuxtLink>
-    <NuxtLink to="/news/sports">Sports</NuxtLink>
-    <NuxtLink to="/news/technology">Technology</NuxtLink>
-    <NuxtLink to="/view/123">View 123</NuxtLink>
-  </nav>
+  <div class="container mx-auto shadow p-3">
+    <h1 class="text-xl">News新聞網</h1>
+    <nav class="flex justify-start space-x-4 overflow-x-scroll mt-3">
+      <NuxtLink
+        v-for="item in router"
+        :key="item.name"
+        :to="item.path"
+        class="whitespace-nowrap font-bold"
+        >{{ item.name }}</NuxtLink
+      >
+      <!-- <NuxtLink to="/view/123">View 123</NuxtLink> -->
+    </nav>
+  </div>
 </template>
 
 <style scoped></style>
